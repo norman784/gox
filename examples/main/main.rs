e133d9a0274ac0356ac5d1359479d2d26c989ee9
norman784 <normanpaniagua@gmail.com>
2019-01-06 10:59:27 +0100

    updated example to use cargo --example

--- examples/main/main.rs
+++ examples/main/main.rs
@@ -8,7 +8,7 @@ use std::{
 use gox::{Gox, Only};
 
 fn main() -> Result<()> {
-    let file = format!("{}/assets/example.gox", env!("CARGO_MANIFEST_DIR"));
+    let file = format!("{}/examples/assets/example.gox", env!("CARGO_MANIFEST_DIR"));
     let mut stream = File::open(&file).unwrap();
     let gox = Gox::new(&mut stream, vec![Only::Layers, Only::Blocks]);
     println!("{:#?}", gox);
